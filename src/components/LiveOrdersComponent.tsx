fedeb74615848d43c2f9348830e84f2abc5e5aac
ANKITROYCHOWDHURY54 <22p31a0504@acet.ac.in>
2025-09-24 19:25:50 +0530

    added

--- src/components/LiveOrdersComponent.tsx
+++ src/components/LiveOrdersComponent.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useState } from 'react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
